196fd479fe02eddca7b66f593062b86ed110863b
SlViKo <41319845+SlViKo@users.noreply.github.com>
2020-04-13 23:53:05 +0300

    Добавьте файлы проекта.

---
+++ WebStore_2020/Controllers/EmployeeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore_2020.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebStore_2020.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        List<EmployeeViewModel> _employeees;
+
+        public EmployeeController()
+        {
+            _employeees = new List<EmployeeViewModel> {
+                new EmployeeViewModel
+                {
+                    Id = 1,
+                    FirstName = "Иван",
+                    SurName = "Иванов",
+                    Patronymic = "Иванович",
+                    Age = 22,
+                    Position = "Начальник"
+                },
+                new EmployeeViewModel
+                {
+                    Id = 2,
+                    FirstName = "Владислав",
+                    SurName = "Петров",
+                    Patronymic = "Иванович",
+                    Age = 35,
+                    Position = "Программист"
+                }
+            };
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            //return Content("Hello from home controller");
+            return View(_employeees);
+        }
+
+        // GET: /<controller>/details/{id}
+        public IActionResult Details(int id)
+        {
+            return View(_employeees.FirstOrDefault(x => x.Id == id));
+        }
+    }
+}
---
+++ WebStore_2020/Startup.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WebStore_2020
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddRouting(options => options.LowercaseUrls = true);
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            //ConfigV22(app, env);
+
+            ConfigV31(app, env);
+        }
+
+        private void ConfigV31(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            var helloMsg = _configuration["CustomHelloWorld"];
+            helloMsg = _configuration["Logging:LogLevel:Default"];
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                              // GET: /<controller>/details/{id}
+
+                //endpoints.MapGet("/", async context => { await context.Response.WriteAsync(helloMsg); });
+            });
+        }
+
+        private void ConfigV22(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Производим конфигурацию инфраструктуры MVC
+            app.UseMvc(routes =>
+            {
+                // Добавляем обработчик маршрута по умолчанию
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+                // Маршрут по умолчанию состоит из трёх частей разделённых “/”
+                // Первой частью указывается имя контроллера,
+                // второй - имя действия (метода) в контроллере,
+                // третей - опциональный параметр с именем “id”
+                // Если часть не указана - используются значения по умолчанию:
+                // для контроллера имя “Home”,
+                // для действия - “Index”
+            });
+
+        }
+    }
+}
