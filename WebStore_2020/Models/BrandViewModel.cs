36227b3f72220043be1bd300e64b513cdd71a181
SlViKo <41319845+SlViKo@users.noreply.github.com>
2020-04-21 11:03:56 +0300

    ViewComponent для брендов

---
+++ WebStore_2020/Models/BrandViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.Models
+{
+    public class BrandViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Order { get; set; }
+    }
+}
---
+++ WebStore_2020/ViewComponents/BrandsViewComponent.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.infrastructure.interfaces;
+using WebStore.Models;
+
+namespace WebStore.ViewComponents
+{
+    public class BrandsViewComponent : ViewComponent
+    {
+        private readonly IProductService _productService;
+
+        public BrandsViewComponent(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var _brands = GetBrands();
+            return View(_brands);
+        }
+
+        private List<BrandViewModel> GetBrands()
+        {
+
+            var brands = _productService.GetBrands().ToArray();
+            var brandsViewModelList = new List<BrandViewModel>();
+
+            foreach (var brand in brands)
+            {
+                brandsViewModelList.Add(new BrandViewModel()
+                {
+                    Id = brand.Id,
+                    Name = brand.Name,
+                    Order = brand.Order
+                });
+            }
+
+            brandsViewModelList = brandsViewModelList.OrderBy(c => c.Order).ToList();
+            return brandsViewModelList;
+        }
+    }
+}
